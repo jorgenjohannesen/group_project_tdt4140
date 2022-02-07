9bb0dd5e44f4de3f2588816272b7e68115e89fb3
magnusrodseth <59113973+magnusrodseth@users.noreply.github.com>
2022-02-07 15:47:53 +0100

    refactor: Refactor test setup

--- backend/tests/app.test.js
+++ backend/tests/app.test.js
@@ -1,26 +1,12 @@
 const fs = require("fs");
-const { setupStrapi } = require("./helpers/strapi");
+const { setupStrapi, tearDownStrapi } = require("./helpers/strapi");
 
 beforeAll(async () => {
   await setupStrapi();
 });
 
 afterAll(async () => {
-  const dbSettings = strapi.config.get("database.connection.connection");
-
-  //close server to release the db-file
-  await strapi.server.destroy();
-
-  //DATABASE_FILENAME=.tmp/test.db
-
-  //delete test database after all tests
-  if (dbSettings && dbSettings.filename) {
-    const tmpDbFile = `${dbSettings.filename}`;
-
-    if (fs.existsSync(tmpDbFile)) {
-      fs.unlinkSync(tmpDbFile);
-    }
-  }
+  await tearDownStrapi();
 });
 
 it("strapi is defined", () => {
--- backend/tests/helpers/strapi.js
+++ backend/tests/helpers/strapi.js
@@ -4,66 +4,31 @@ let instance;
 
 async function setupStrapi() {
   if (!instance) {
-    /** the following code in copied from `./node_modules/strapi/lib/Strapi.js` */
     instance = await Strapi().load();
     instance.server.mount(); // strapi is global now
-    // await instance.server
-    //   .use(instance.server.router.routes()) // populate KOA routes
-    //   .use(instance.server.router.allowedMethods()); // populate KOA methods
 
     instance = instance.server.app.callback();
   }
   return instance;
 }
 
-async function setPublicPermissions(newPermissions) {
-  // Find the ID of the public role
-  const publicRole = await strapi
-    .query("plugin::users-permissions.role")
-    .findOne({
-      where: {
-        type: "public",
-      },
-    });
+async function tearDownStrapi() {
+  const dbSettings = strapi.config.get("database.connection.connection");
 
-  // Create the new permissions and link them to the public role
-  const allPermissionsToCreate = [];
-  Object.keys(newPermissions).map((controller) => {
-    const actions = newPermissions[controller];
-    const permissionsToCreate = actions.map((action) => {
-      return strapi.query("plugin::users-permissions.permission").create({
-        data: {
-          action: `api::${controller}.${controller}.${action}`,
-          role: publicRole.id,
-        },
-      });
-    });
-    allPermissionsToCreate.push(...permissionsToCreate);
-  });
-  await Promise.all(allPermissionsToCreate);
-}
+  //close server to release the db-file
+  await strapi.server.destroy();
 
-const grantPrivileges = async (roleID = 1, values = []) => {
-  const roles = await strapi
-    .service("plugin::users-permissions.role")
-    .getRoles();
-  const _public = await strapi
-    .service("plugin::users-permissions.role")
-    .getRole(roles.filter((role) => role.type === "public")[0].id);
+  //delete test database after all tests
+  if (dbSettings && dbSettings.filename) {
+    const tmpDbFile = `${dbSettings.filename}`;
 
-  for (const i in values) {
-    _public.permissions[values[i]].controllers[
-      values[i].split("::")[1]
-    ].find.enabled = true;
+    if (fs.existsSync(tmpDbFile)) {
+      fs.unlinkSync(tmpDbFile);
+    }
   }
-
-  await strapi
-    .service("plugin::users-permissions.role")
-    .updateRole(_public.id, _public);
-};
+}
 
 module.exports = {
   setupStrapi,
-  grantPrivileges,
-  setPublicPermissions,
+  tearDownStrapi,
 };
