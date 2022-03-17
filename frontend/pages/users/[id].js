988ffa275a812962e1d3892bea5749c3902ff613
sindre <sindre.e@live.com>
2022-03-17 14:13:32 +0100

    attempts to make user page more responsive

--- frontend/pages/users/[id].js
+++ frontend/pages/users/[id].js
@@ -22,40 +22,48 @@ const User = ({ yourHikes, participatedHikes, owner, profilePictureURL }) => {
 
   return (
     <Grid container spacing={8} justifyContent="flex-start" alignItems="center" direction="column">
-      <Grid container item alignItems="center" direction="column">
-        <Grid item xs={5}>
-          <Container>
-            <div style={{ borderRadius:"50%", overflow: 'hidden', width:350, height:350}}>
+      <Grid container item xs={5} alignItems="center" direction="column">
+        <Grid item container xs={5}>
+          <Container sx={{
+            width: "350px",
+            maxWidth: "50%",
+          }}>
+            <Box sx={{
+              borderRadius: "50%",
+              overflow: 'hidden',
+              borderStyle: "solid",
+              borderColor: "black",
+              borderWidth: "1px",
+            }}>
               <Image
                 src={profilePictureURL || defaultProfilePicture}
                 height={350}
                 width={350}
-                layout="intrinsic"
+                layout="responsive"
               />
-            </div>
+            </Box>
           </Container>
         </Grid>
         <Grid item xs={4}>
           <Container>
-            <Typography variant="h1" sx={{textAlign:"center"}}>{username}</Typography>
+            <Typography variant="h2" sx={{textAlign:"center", fontSize: "10vmin"}}>{username}</Typography>
           </Container>
         </Grid>
       </Grid>
-
       <Grid container xs={6}>
         <Container>
           {isCommercial && description && (
             <Box>
-              <Typography variant="h5">Who is {username}?</Typography>
-              <Typography variant="subtitle1">{description}</Typography>
+              <Typography variant="h5" sx={{fontSize: "5vmin"}}>Who is {username}?</Typography>
+              <Typography variant="subtitle1" sx={{fontSize: "2vmin"}}>{description}</Typography>
             </Box>
           )}
         </Container>
       </Grid>
-      <Grid container item xs={12} justifyContent="center" alignItems="flex-start">
+      <Grid container item xs={12} rowSpacing={5} justifyContent="center" alignItems="flex-start">
         <Grid container item sm={12} md={6} direction="column" alignItems="center" justifyContent="flex-start">
           <Grid item>
-            <Typography variant="h4">
+            <Typography variant="h4" sx={{fontSize: "4vmin"}}>
               {ownerId == userId ? "My " : `${username}'s`} hikes
             </Typography>
           </Grid>
@@ -63,7 +71,7 @@ const User = ({ yourHikes, participatedHikes, owner, profilePictureURL }) => {
         </Grid>
         {participatedHikes.length > 0 &&
         <Grid container item sm={12} md={6} direction="column" alignItems="center" justifyContent="flex-start">
-          <Typography variant="h4">
+          <Typography variant="h4" sx={{fontSize: "4vmin"}}>
             Hikes {ownerId == userId ? "I have" : `${username} has`}{" "}
             participated in
           </Typography>
